702bbe2d1ea848baba497733c954c8145528b917
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-12 17:50:13 +0100

    feat(icon): add favicon

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Github, Twitter } from "lucide-react";
+import Link from "next/link";
 
 interface HeaderProps {
   className?: string;
@@ -6,8 +8,18 @@ interface HeaderProps {
 
 const Header = ({ className }: HeaderProps) => {
   return (
-    <div className={`${className}`}>
-      <h1>Header works!</h1>
+    <div
+      className={`${className} w-full flex items-center md:justify-around justify-between my-4 p-3`}
+    >
+      <h1 className="font-extrabold text-[22px]">Linq</h1>
+      <section className="flex gap-x-5">
+        <Link href={"https://github.com/adedoyin-emmanuel/linq"}>
+          <Github className="w-5 h-5 cursor-pointer" />
+        </Link>
+        <Link href={"https://x.com/Emmysoft_Tm"}>
+          <Twitter className="w-5 h-5 cursor-pointer" />
+        </Link>
+      </section>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Linq",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={grotesk.className}>{children}</body>
     </html>
   );
 }
