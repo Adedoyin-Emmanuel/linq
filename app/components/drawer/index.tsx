6a4e34af6802fb86ad84e8cef63a3811c6121a33
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-12 18:29:55 +0100

    feat(components): added custom drawer

--- app/components/drawer/index.tsx
+++ app/components/drawer/index.tsx
@@ -1,12 +1,77 @@
 "use client";
 import React from "react";
+import { useMediaQuery } from "usehooks-ts";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+
+import {
+  Dialog,
+  DialogDescription,
+  DialogTitle,
+  DialogContent,
+  DialogHeader,
+} from "@/components/ui/dialog";
 
 interface DrawerProps {
   className?: string;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: any;
+  hideCloseButton?: boolean;
+  customFooterChildren?: React.ReactNode;
 }
 
-const Drawer = ({ className }: DrawerProps) => {
-  return <div className={`${className}`}></div>;
+const ClipsaveDrawer = ({
+  className,
+  title,
+  description,
+  isOpen,
+  children,
+  setIsOpen,
+  hideCloseButton,
+  customFooterChildren,
+}: DrawerProps) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="text-start">{title}</DialogTitle>
+            <DialogDescription className="text-start">
+              {description}
+            </DialogDescription>
+          </DialogHeader>
+          <div className={`${className}`}>{children}</div>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={isOpen} onOpenChange={setIsOpen}>
+      <DrawerContent>
+        <div className="mx-auto w-full ">
+          <DrawerHeader>
+            <DrawerTitle className="text-start">{title}</DrawerTitle>
+            <DrawerDescription className="text-start">
+              {description}
+            </DrawerDescription>
+          </DrawerHeader>
+
+          <div className={`p-4 pb-0 ${className}`}>{children}</div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 };
 
-export default Drawer;
+export default ClipsaveDrawer;
