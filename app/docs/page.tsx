4ad230a849cbf0a450703cebad25e28a5b9b4e9b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-12 20:10:10 +0100

    feat: finshed docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,16 +1,50 @@
-"use client";
-import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+import Header from "@/app/components/header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const Docs = () => {
-  const pathname = usePathname();
-
+export default function Docs() {
   return (
-    <div>
-      <h1>Docs works!</h1>
-      <p>Current pathname: {pathname}</p>
-    </div>
-  );
-};
+    <section className="w-full flex items-center justify-around flex-col">
+      <Header />
+      <br></br>
+      <section className="w-full max-w-[600px]  flex flex-col items-center  justify-center my-4 p-3">
+        <h2 className="font-extrabold text-[20px] w-full text-start my-2">
+          About Me | Docs
+        </h2>
+        <p className="text-justify">
+          Hi there, I'm Adedoyin Emmanuel Adeniyi, I'm from Lagos Nigeria 🇳🇬.
+          I'm a Fullstack Engineer with over {new Date().getFullYear() - 2020}{" "}
+          years of experience. I love building open-source projects and sharing
+          stuff I learn online through my{" "}
+          <Link href={"https://adedoyin.hashnode.dev"} className="underline">
+            Blog{" "}
+          </Link>{" "}
+          or{" "}
+          <Link href={"https://x.com/Emmysoft_Tm"} className="underline">
+            Twitter.{" "}
+          </Link>
+          I built <strong>Linq</strong> to help users construct social sharable
+          links from their text. I had this issue while building the{" "}
+          <Link href={"https://clipsave.space/support"} className="underline">
+            support{" "}
+          </Link>
+          page for{" "}
+          <Link href={"https://clipsave.space/app"} className="underline">
+            Clipsave.{" "}
+          </Link>
+          If you find this project useful, you can give it a star on{" "}
+          <Link
+            href={"https://github.com/adedoyin-emmanuel/linq"}
+            className="underline"
+          >
+            Github{" "}
+          </Link>
+          . Thank you for using my product 🙌
+        </p>
 
-export default Docs;
+        <br />
+        <Button className="w-full p-6 my-3">Connect with me</Button>
+      </section>
+    </section>
+  );
+}
